eda288097a90ed2ab0f8ef016a238c4e76aabddf
sabina165 <58961533+sabina165@users.noreply.github.com>
2023-04-19 19:50:19 +0200

    Add other data input

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,3 +49,7 @@ my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
 my_data_row = my_cur.fetchall()
 streamlit.header("the fruit list contain")
 streamlit.dataframe(my_data_row)
+
+# Add other data input
+add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
+streamlit.write('Thanks for adding jackfruit ', add_my_fruit )
